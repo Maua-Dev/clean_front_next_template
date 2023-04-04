6b4d537576545d7f798f157bfc912ff13398c5ca
Rodrigo <rodrigo.dsiqueira1@gmail.com>
2023-04-04 14:12:43 -0300

    added STATE enum and logic with this enum

--- src/@clean/shared/domain/enums/state_enum.ts
+++ src/@clean/shared/domain/enums/state_enum.ts
@@ -3,3 +3,16 @@ export enum STATE {
     PENDING = "PENDING",
     REJECTED = "REJECTED"
 };
+
+export function toEnum(value: string): STATE {
+    switch (value) {
+        case "APPROVED":
+            return STATE.APPROVED;
+        case "PENDING":
+            return STATE.PENDING;
+        case "REJECTED":
+            return STATE.REJECTED;
+        default:
+            throw new Error("Invalid value");
+    }
+}
