4ac7538a45803beb073a62c347afac86cfd65f54
Rodrigo <rodrigo.dsiqueira1@gmail.com>
2023-04-04 23:18:05 -0300

    added delete user page

---
+++ src/app/delete_user/page.tsx
@@ -0,0 +1,34 @@
+'use client';
+import { UserContext } from '@/contexts/user_provider';
+import React, { useContext, useEffect } from 'react';
+import styles from '../users/users.module.css';
+import styles_delete_user from './delete_user.module.css';
+
+export default function DeleteUser() {
+    const { users, deleteUser } = useContext(UserContext)
+
+    function handleDeleteUser(id: string) {
+        deleteUser(id);
+
+        console.log('User deleted: ', id);
+    }
+
+    return (
+        <div>
+            <h1>Delete User</h1>
+
+            <div className={ styles.page__container }>
+            { users.map((user) => {
+                return (
+                    <div key={user.id} className={ styles.container__users }>
+                        <p className={ styles.name__user } >{`User: ${user.name}`}</p>
+                        <p className={ styles.email__user } >{`User: ${user.email}`}</p>
+                        <p className={ styles.state__user } >{`User: ${user.state}`}</p>
+                        <button onClick={() => handleDeleteUser(user.id)}  className={ styles_delete_user.button__submit } >Delete user</button>
+                    </div>
+                )
+            }) }
+            </div>
+        </div>
+    )
+}
