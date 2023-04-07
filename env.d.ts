6e8cdba18b6bf683331ae1c1ba5347018cef7bf4
Rodrigo <rodrigo.dsiqueira1@gmail.com>
2023-04-07 15:22:45 -0300

    setup files

---
+++ env.d.ts
@@ -0,0 +1,9 @@
+namespace NodeJS {
+    interface ProcessEnv {
+        NEXT_PUBLIC_SUPABASE_URL: string;
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
+        NEXT_PUBLIC_STAGE: string;
+    }
+}
+
+ProcessEnv
