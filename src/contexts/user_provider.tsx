0f8ea480575e1f9b4a6fd392b06cf7ecc29afe23
Rodrigo <rodrigo.dsiqueira1@gmail.com>
2023-04-18 13:07:42 -0300

    added error and set error in provider

--- src/contexts/user_provider.tsx
+++ src/contexts/user_provider.tsx
@@ -1,65 +1,94 @@
 'use client';
-import { createContext, PropsWithChildren, useEffect, useState } from "react";
+import { createContext, PropsWithChildren, useState } from "react";
 import { User } from "@/@clean/shared/domain/entities/user";
 import { container, Registry } from "@/@clean/shared/infra/containers/container_user";
-import { GetUsersUsecase } from "@/@clean/modules/user/usecases/get_users_usecase";
+import { GetUserUsecase } from "@/@clean/modules/user/usecases/get_user_usecase";
 import { CreateUserUsecase } from "@/@clean/modules/user/usecases/create_user_usecase";
 import { UpdateUserUsecase } from "@/@clean/modules/user/usecases/update_user_usecase";
 import { DeleteUserUsecase } from "@/@clean/modules/user/usecases/delete_user_usecase";
+import { AxiosError } from "axios";
+import { NoItemsFoundError } from "@/@clean/shared/domain/helpers/errors/domain_error";
 
 export type UserContextType = {
     users: User[];
     createUser: (user: User) => void;
-    getUsers: () => void;
-    updateUser: (id: string, newName: string) => void;
-    deleteUser: (id: string) => void;
+    getUser: (userId: number) => void;
+    updateUser: (id: number, newName: string) => void;
+    deleteUser: (id: number) => void;
+    error: NoItemsFoundError | null;
+    setErrorNull: () => void;
 }
 
 
 const defaultContext: UserContextType = {
     users: [],
     createUser: (user: User) => {},
-    getUsers: () => {},
-    updateUser: (id: string, newName: string) => {},
-    deleteUser: (id: string) => {}
+    getUser: (userId: number) => {},
+    updateUser: (id: number, newName: string) => {},
+    deleteUser: (id: number) => {},
+    error: null,
+    setErrorNull: () => {}
 }
 
 export const UserContext = createContext(defaultContext);
 
-const getUserUsecase = container.get<GetUsersUsecase>(Registry.GetUsersUsecase)
+const getUserUsecase = container.get<GetUserUsecase>(Registry.GetUsersUsecase)
 const createUserUseCase = container.get<CreateUserUsecase>(Registry.CreateUserUsecase)
 const updateUserUseCase = container.get<UpdateUserUsecase>(Registry.UpdateUserUsecase)
 const deleteUserUseCase = container.get<DeleteUserUsecase>(Registry.DeleteUserUsecase)
 
 export function UserProvider({ children }: PropsWithChildren) {
     const [users, setUsers] = useState<User[]>([]);
+    // State for error in API
+    // const [error, setError] = useState<AxiosError | Error | null>(null);
+    const [error, setError] = useState<NoItemsFoundError | null>(null);
 
-    function createUser(user: User) {
-        const userCreated = createUserUseCase.execute(user)
+    async function createUser(user: User) {
+        const userCreated = await createUserUseCase.execute(user)
         setUsers([...users, userCreated])
     }
 
-    function getUsers() {
-        const allUsers = getUserUsecase.execute();
-        setUsers([...allUsers])
+    async function getUser(userId: number) {
+        try {
+            const userGetted = await getUserUsecase.execute(userId);
+            setUsers([...users, userGetted])
+        } catch (error: any) {
+            console.log(`ERROR PROVIDER: ${error}`)
+            const setNotFoundError = new NoItemsFoundError(`id`)
+            setError(setNotFoundError)
+        }
     }
 
-    function updateUser(userId: string, newName: string) {        
-        updateUserUseCase.execute(userId, newName)
-        getUsers()
+    async function updateUser(userId: number, newName: string) {        
+        await updateUserUseCase.execute(userId, newName)
+        const userUpdated = await getUserUsecase.execute(userId)
+        users.filter(user => user.id !== userId)
+        setUsers([...users, userUpdated])
     }
 
-    function deleteUser(userId: string) {
-        deleteUserUseCase.execute(userId)
-        getUsers()
+    async function deleteUser(userId: number) {
+        await deleteUserUseCase.execute(userId)
+        const userDeleted = await getUserUsecase.execute(userId)
+        users.filter(user => user.id !== userId)
+        setUsers([...users, userDeleted])
+
     }
 
-    useEffect(() => {
-        getUsers()
-    }, [])
+    function setErrorNull() {
+        setError(null)
+    }
 
     return (
-        <UserContext.Provider value={{ users, createUser, getUsers, updateUser, deleteUser }}>
+        <UserContext.Provider 
+            value={{ users, 
+                    createUser, 
+                    getUser, 
+                    updateUser, 
+                    deleteUser, 
+                    error, 
+                    setErrorNull 
+                }}
+        >
             { children }
         </UserContext.Provider>
     )
