7b973d6caecff81fb281ee573f900c7342db6fc6
Rodrigo <rodrigo.dsiqueira1@gmail.com>
2023-04-18 12:52:29 -0300

    setted url in .env and now using in http.ts

--- env.d.ts
+++ env.d.ts
@@ -3,7 +3,6 @@ namespace NodeJS {
         NEXT_PUBLIC_SUPABASE_URL: string;
         NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
         NEXT_PUBLIC_STAGE: string;
+        NEXT_PUBLIC_API_URL: string;
     }
 }
-
-ProcessEnv
